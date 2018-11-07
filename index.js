ae43f5b3c72e0edaf290cc9d775ae9a0163a1cae
krish kandula <krishna.kandula@tekzenit.com>
2018-11-07 00:01:34 -0600

    npx support

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+#!/usr/bin/env node
+var path = require('path');
 var player = require('play-sound')(opts = {})
 
-player.play('fasak-audio.mov', function(err){
+player.play(path.join(__dirname + '/fasak-audio.mov'), function(err){
     if (err) throw err
 })
